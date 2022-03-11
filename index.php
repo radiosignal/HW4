42a13c57373aa9046a7e9d44f59e4413dbea800b
radiosignal <100569122+radiosignal@users.noreply.github.com>
2022-03-11 13:45:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$dirImages = 'img/';
+$images = scandir($dirImages);
+for ($i = 0; $i < count($images); $i++) {
+    if ($images[$i] != '.' && $images[$i] != '..') {
+        echo '<a href="' . $dirImages . $images[$i] . '" target="_blank"><img src=' . $dirImages . $images[$i] . ' style="width: 50px;"></a>';
+    }
+}
+
